f534228b594256b161466278f2c5d40b423445c5
Gaudan90 <daniele.gauzolino@edu.itspiemonte.it>
2025-01-17 22:09:52 +0100

    chore: removed unused code

--- ui/navigation/tab/tab.navigator.tsx
+++ ui/navigation/tab/tab.navigator.tsx
@@ -7,22 +7,8 @@ import FavoritesScreen from '../../screens/favorites/favorites.screen';
 import ProductDetailScreen from '../../screens/details/product.detail.screen';
 import FiltersScreen from '../../screens/filters/filter.screen';
 import { Ionicons } from '@expo/vector-icons';
-import { useRoute } from '@react-navigation/native';
-import { Product } from '../../types/product.types';
-import { View } from 'react-native';
 
 const Tab = createBottomTabNavigator<TabParamList>();
-
-const ProductDetailTabButton = ({ children, ...props }: any) => {
-  const route = useRoute();
-  const params = route.params as { product?: Product };
-  
-  if (!params?.product) {
-    return null;
-  }
-  
-  return children;
-};
 
 const TabNavigator = () => {
   return (
