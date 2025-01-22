196375d285f7df43a8ff9b2026140bd98088577f
Gaudan90 <daniele.gauzolino@edu.itspiemonte.it>
2025-01-22 20:41:31 +0100

    fix: fixed the emitter as type

--- ui/utilities/event.emitter.ts
+++ ui/utilities/event.emitter.ts
@@ -1,6 +1,33 @@
-import { EventEmitter } from 'events';
+type Listener = (...args: any[]) => void;
 
-const favoritesEventEmitter = new EventEmitter();
+class CustomEventEmitter {
+  private listeners: { [event: string]: Listener[] } = {};
+
+  on(event: string, listener: Listener): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(listener);
+  }
+
+  emit(event: string, ...args: any[]): void {
+    const eventListeners = this.listeners[event];
+    if (eventListeners) {
+      eventListeners.forEach(listener => listener(...args));
+    }
+  }
+
+  removeListener(event: string, listenerToRemove: Listener): void {
+    const eventListeners = this.listeners[event];
+    if (eventListeners) {
+      this.listeners[event] = eventListeners.filter(
+        listener => listener !== listenerToRemove
+      );
+    }
+  }
+}
+
+const favoritesEventEmitter = new CustomEventEmitter();
 export const FAVORITES_UPDATED = 'favoritesUpdated';
 
 export default favoritesEventEmitter;
